99fb96e089eb847c6c2abb2ada257ae69f35e09e
AG18a <antoine.gelles@gastonberger.fr>
2025-12-15 15:47:10 +0100

    Update script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+
+document.querySelectorAll('nav#navbar a').forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href'))
+                .scrollIntoView({ behavior: 'smooth' });
+    });
+});
+
+// Form alert on submit
+document.getElementById("contact-form").addEventListener("submit", function(e) {
+    e.preventDefault();
+    alert("Merci pour votre message !");
+});
